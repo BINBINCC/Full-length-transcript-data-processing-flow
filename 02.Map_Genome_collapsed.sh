bbd8aac05b4712ddf88f883cc1883487af40a40e
BINBINCC <54542217+BINBINCC@users.noreply.github.com>
2022-02-15 10:37:34 +0800

    Create 02.Map_Genome_collapsed.sh

---
+++ 02.Map_Genome_collapsed.sh
@@ -0,0 +1,8 @@
+##Alignment of PacBio reads to genome
+minimap2 -ax splice -uf -C5  -t 10 Brapa_sequence_v3.0.fasta flnc.correct.fasta > flnc.correct.sam
+##Unique alignment
+cat flnc.correct.sam |grep -v @ | grep -v -w -E '256|272|2048|2064' | awk '{if($2 != 4) print $0}' > flnc.correct.uniq.sam
+##Collapse redundant isoforms
+collapse_isoforms_by_sam.py --input flnc.correct.fasta -s flnc.correct.sam --max_3_diff 500 --min-coverage 0.90 --min-identity 0.90 -o flnc.correct.collapsed
+##GFF to GFF3
+gffread flnc.correct.collapsed.gff -o- > flnc.correct.collapsed.gff3
